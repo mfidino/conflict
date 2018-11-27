69dbac0e804276916459729f28710e699b3b0864
Mason Fidino <mfidino@lpzoo.org>
2018-11-26 20:27:34 -0600

    finished integrated model
    The po detection function is not working  correctly.

--- sim_utility.R
+++ sim_utility.R
@@ -371,6 +371,7 @@ inits <- function(chain){
 }
 
 
+
 my_vioplot <- function (x, ..., range = 1.5, h = NULL, ylim = NULL, names = NULL, 
 												horizontal = FALSE, col = "magenta", border = "black", lty = 1, 
 												lwd = 1, rectCol = "black", colMed = "white", pchMed = 19, 
